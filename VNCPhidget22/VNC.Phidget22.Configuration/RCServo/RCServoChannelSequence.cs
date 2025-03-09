90cc0aa21666f20c16f3c78020247de9fcfad4ab
Christopher Rhodes <chrhodes@cox.net>
2025-03-09 16:04:40 -0700

    Now have ChannelClass Sequences in PerformanceSelectorControl.  UI now shows available (attached) Phidgets that support ChannelClass.  No need to select host.  Next step is to get ChannelSequence to execute

---
+++ VNCPhidget22/VNC.Phidget22.Configuration/RCServo/RCServoChannelSequence.cs
@@ -0,0 +1,16 @@
+﻿using VNC.Phidget22.Configuration.Performance;
+
+namespace VNC.Phidget22.Configuration
+{
+    public class RCServoChannelSequence : ChannelClassSequence
+    {
+        public RCServoChannelSequence() : base("RCServo")
+        {
+        }
+
+        /// <summary>
+        /// Array of RCServo actions for channel
+        /// </summary>
+        public RCServoAction[]? Actions { get; set; }
+    }
+}
