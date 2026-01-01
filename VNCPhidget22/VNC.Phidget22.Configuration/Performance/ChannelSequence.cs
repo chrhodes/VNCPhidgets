feda64d19b8398636559bd3ce6b443bef22d8a75
Christopher Rhodes <chrhodes@cox.net>
2025-12-31 17:17:47 -0800

    That's a wrap for 2025.  All Skulls, Arms, Hands working with new servos

--- VNCPhidget22/VNC.Phidget22.Configuration/Performance/ChannelSequence.cs
+++ VNCPhidget22/VNC.Phidget22.Configuration/Performance/ChannelSequence.cs
@@ -120,4 +120,123 @@ namespace VNC.Phidget22.Configuration.Performance
         /// </summary>
         public DeviceChannelSequence? NextSequence { get; set; }
     }
+
+    public class CopyOfChannelSequence
+    {
+        private static readonly object _lock = new object();
+
+        public CopyOfChannelSequence(string channelClass)
+        {
+            ChannelClass = channelClass;
+        }
+
+        public CopyOfChannelSequence(string channelClass, CopyOfChannelSequence channelSequence)
+        {
+            lock (_lock)
+            {
+                ChannelClass = channelClass;
+
+                Name = channelSequence.Name;
+                Description = channelSequence.Description;
+                UsageNotes = channelSequence.UsageNotes;
+                HubPort = channelSequence.HubPort;
+                Channel = channelSequence.Channel;
+                BeforeActionLoopSequences = channelSequence.BeforeActionLoopSequences;
+                StartActionLoopSequences = channelSequence.StartActionLoopSequences;
+                ActionLoops = channelSequence.ActionLoops;
+                ExecuteActionsInParallel = channelSequence.ExecuteActionsInParallel;
+                ActionsDuration = channelSequence.ActionsDuration;
+                EndActionLoopSequences = channelSequence.EndActionLoopSequences;
+                SequenceLoops = channelSequence.SequenceLoops;
+                AfterActionLoopSequences = channelSequence.AfterActionLoopSequences;
+                SequenceDuration = channelSequence.SequenceDuration;
+                NextSequence = channelSequence.NextSequence;
+            }
+        }
+
+        /// <summary>
+        /// Name of sequence. Must be unique in ChannelClass
+        /// Can be null if using embedded CopyOfChannelSequence
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Description of CopyOfChannelSequence (optional)
+        /// </summary>
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// Notes on using CopyOfChannelSequence (optional)
+        /// </summary>
+        public string? UsageNotes { get; set; }
+
+        /// <summary>
+        /// ChannelClass.  Set in constructor.
+        /// </summary>
+        public string ChannelClass { get; } = "Generic";
+
+        /// <summary>
+        /// HubPort on Phidget Device.
+        /// If null, must be overridden in DeviceChannelSequence
+        /// </summary>
+        public Int32? HubPort { get; set; } = null;
+
+        /// <summary>
+        /// Channel on Phidget Device.
+        /// If null, must be overridden in DeviceChannelSequence 
+        /// </summary>
+        public Int32? Channel { get; set; } = null;
+
+        /// <summary>
+        /// PerformanceSequence[] to call before executing Actions
+        /// </summary>
+        public DeviceChannelSequence[]? BeforeActionLoopSequences { get; set; }
+
+        /// <summary>
+        /// PerformanceSequence[] to call at start of each SequenceLoop
+        /// </summary>
+        public DeviceChannelSequence[]? StartActionLoopSequences { get; set; }
+
+        /// <summary>
+        /// Number of loops of sequence Actions[] to execute
+        /// </summary>
+        public Int32 ActionLoops { get; set; } = 1;
+
+        /// <summary>
+        /// Play Actions[] in Parallel or Sequentially (false)
+        /// </summary>
+        public Boolean ExecuteActionsInParallel { get; set; } = false;
+
+        /// <summary>
+        /// Duration of Action[] in ms (sleep time after Action[] completed)
+        /// </summary>
+        public Int32? ActionsDuration { get; set; }
+
+        /// <summary>
+        /// PerformanceSequence[] to call at start of each SequenceLoop
+        /// </summary>
+        public DeviceChannelSequence[]? EndActionLoopSequences { get; set; }
+
+        /// <summary>
+        /// Number of loops of sequence to execute
+        /// </summary>
+        public Int32 SequenceLoops { get; set; } = 1;
+
+        /// <summary>
+        /// PerformanceSequence[] to call after executing Actions
+        /// before calling NextSequence
+        /// </summary>
+        public DeviceChannelSequence[]? AfterActionLoopSequences { get; set; }
+
+        /// <summary>
+        /// Duration of Action[] in ms (sleep time after Actions completed)
+        /// </summary>
+        public Int32? SequenceDuration { get; set; }
+
+        /// <summary>
+        /// PerformanceSequence to invoke at end of sequence loops (optional)
+        /// none or null to stop
+        /// </summary>
+        public DeviceChannelSequence? NextSequence { get; set; }
+    }
 }
